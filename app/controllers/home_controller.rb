9740679499f28ea9faf46506a1a915285969af65
Daniel Riley <driley3@gmail.com>
2016-09-16 16:20:43 -0400

    ssl cert added with letsencrypt

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   end
 
   def letsencrypt
-    render text: "SZXip-z1jj-Db7azz82pos5aNXITeXqK2FxovK2e_3s.xJjr_1FvLKOavDWse3T6xPQibiy1zjS3zVMOrsRx0RM"
+    render text: "o7cStDrMPvBARH-A6tOSEZyRZeaSVdGTwEOJFwrgd9I.xJjr_1FvLKOavDWse3T6xPQibiy1zjS3zVMOrsRx0RM"
   end
-  
+
 end
