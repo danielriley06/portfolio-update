1b3f49ed8d0cac5b85eb3690f6c6ae0e16aabf35
Daniel Riley <driley3@gmail.com>
2016-09-16 16:07:53 -0400

    ssl cert added with letsencrypt

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,8 @@ class HomeController < ApplicationController
   def index
   end
 
+  def letsencrypt
+    render text: "SZXip-z1jj-Db7azz82pos5aNXITeXqK2FxovK2e_3s.xJjr_1FvLKOavDWse3T6xPQibiy1zjS3zVMOrsRx0RM"
+  end
+  
 end
